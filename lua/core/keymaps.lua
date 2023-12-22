4e92d4bc0f6d46212f1e7757202c7f9c65ea8365
zachbharris <zachbharris94@gmail.com>
2023-12-21 21:46:41 -0600

    changes to lsp, new keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,7 +1,23 @@
+vim.g.mapleader = " "
+
 local keymap = vim.keymap.set
 
+-- move selection up/down
 keymap("v", "J", ":m '>+1<CR>gv=gv")
 keymap("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true, silent = true })
 
+vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+
+-- general keymaps
+keymap("n", "x",'"_x')
+keymap("n", "<leader>+", "<C-a>")
+keymap("n", "<leader>-", "<C-x>")
+
+keymap("n", "<leader>sv", "<C-w>v", { desc = "split vertical" })
+keymap("n", "<leader>sh", "<C-w>s", { desc = "split horizontal" })
+keymap("n", "<ledaer>se", "<C-w>=", { desc = "make split windows equal width" })
+keymap("n", "<leader>sx", ":close<CR>")
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -19,14 +19,9 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
       lspconfig.tsserver.setup({})
       lspconfig.astro.setup({})
       lspconfig.tailwindcss.setup({})
-      lspconfig.html.setup({})
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
     end
   }
 }
