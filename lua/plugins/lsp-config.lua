bac8f6347945c3d960f3842c61d293dc1de56b98
zachbharris <zachbharris94@gmail.com>
2024-01-02 23:22:34 -0600

    lsp changes

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -35,11 +35,15 @@ return {
       lspconfig.gopls.setup({
         capabilities = capabilities,
       })
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities,
+      })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, {})
     end,
   },
 }
