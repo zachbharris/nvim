48fec77de3722e8e7f673d43aa7dd8e65352be65
zachbharris <zachbharris94@gmail.com>
2024-01-04 21:38:46 -0600

    changed tree keybind to C-n

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -31,8 +31,8 @@ return {
 					end,
 				},
 				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					-- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					-- ["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = false }),
