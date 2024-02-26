fef46e80bbde6ae647edc9f67723c1551bc1ca84
zachbharris <zachbharris94@gmail.com>
2024-02-25 21:01:33 -0600

    change theme to nightfox-carbonfox

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,13 +1,9 @@
 return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				flavour = "mocha",
-			})
-			vim.cmd.colorscheme("catppuccin")
-		end,
-	},
+  "EdenEast/nightfox.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("nightfox").setup()
+    vim.cmd("colorscheme carbonfox")
+  end,
 }
