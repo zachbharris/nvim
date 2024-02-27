8a78e9e61a855a90d9b8c9d1f33f49f5c8753295
zachbharris <zachbharris94@gmail.com>
2024-02-26 18:31:04 -0600

    fix neo-tree bug for focusing current buffer, remove lazy loading from package info

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -10,7 +10,7 @@ return {
 		require("neo-tree").setup({
 			filesystem = {
 				follow_current_file = {
-					enable = true,
+					enabled = true,
 				},
 			},
 		})
--- lua/plugins/package-info.lua
+++ lua/plugins/package-info.lua
@@ -1,6 +1,5 @@
 return {
 	"vuki656/package-info.nvim",
-  lazy = true,
 	config = function()
 		require("package-info").setup()
 	end,
