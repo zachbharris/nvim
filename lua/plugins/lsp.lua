713d6a32115bb4d5865854ecb2f26f1af043346e
zachbharris <zachbharris94@gmail.com>
2024-02-26 18:38:54 -0600

    formatting lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,79 +1,79 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		lazy = false,
-		dependencies = {
-			"rafamadriz/friendly-snippets",
-			"saadparwaiz1/cmp_luasnip",
-		},
-		config = function()
-			require("luasnip.loaders.from_vscode").lazy_load()
-		end,
-	},
-	{
-		"hrsh7th/cmp-nvim-lsp",
-		lazy = false,
-		config = true,
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		lazy = false,
-		config = function()
-			local cmp = require("cmp")
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    lazy = false,
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+  },
+  {
+    "hrsh7th/cmp-nvim-lsp",
+    lazy = false,
+    config = true,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    lazy = false,
+    config = function()
+      local cmp = require("cmp")
 
-			cmp.setup({
-				window = {
-					documentation = cmp.config.window.bordered(),
-					completion = cmp.config.window.bordered(),
-				},
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-Space"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = false }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "copilot" },
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local cap = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
+      cmp.setup({
+        window = {
+          documentation = cmp.config.window.bordered(),
+          completion = cmp.config.window.bordered(),
+        },
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        }),
+        sources = cmp.config.sources({
+          { name = "copilot" },
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local cap = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
 
-			lspconfig.tsserver.setup({ capabilities = cap })
-			lspconfig.html.setup({ capabilities = cap })
-			lspconfig.lua_ls.setup({ capabilities = cap })
-			lspconfig.astro.setup({ capabilities = cap })
-			lspconfig.gopls.setup({ capabilities = cap })
-			lspconfig.tailwindcss.setup({ capabilities = cap })
-			lspconfig.eslint.setup({ capabilities = cap })
-		end,
-	},
+      lspconfig.tsserver.setup({ capabilities = cap })
+      lspconfig.html.setup({ capabilities = cap })
+      lspconfig.lua_ls.setup({ capabilities = cap })
+      lspconfig.astro.setup({ capabilities = cap })
+      lspconfig.gopls.setup({ capabilities = cap })
+      lspconfig.tailwindcss.setup({ capabilities = cap })
+      lspconfig.eslint.setup({ capabilities = cap })
+    end,
+  },
 }
